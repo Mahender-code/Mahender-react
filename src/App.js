77b2d25e87b8ba8ae7c7b6c6a3da3695104bdb3c
mahender <mahender.sarampally@gmail.com>
2021-05-03 21:57:16 +0530

    component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>HElloWorld</h1>
-      <h2>HEllo Telangana</h2>
-      <h3>Hello Wporld</h3>
-      <h4>jffjfjf</h4>
+      <Check></Check>
     </div>
   );
 }
